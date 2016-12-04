7d6f4d78d77531e3ee4a630b01c3482ef2e1c7cd
Bogdan Pshonyak <bpshonyak@live.com>
2016-12-03 18:14:53 -0800

    code to load initial ideas done... time to test it

--- static/js/init.js
+++ static/js/init.js
@@ -12,13 +12,46 @@ window.onload = function () {
 
     Materialize.updateTextFields();
 
+    $ideasContainer = $("#ideas");
+
     $.get( "/ideas", function( data ) {
       data.entries.forEach(function (idea) {
-        console.log(idea); // array of values
+        var ideaHTML = generateIdeaRow(idea);
+        $ideasContainer.append(ideaHTML);
       });
     });
 
     var ideaForm = $("#ideaForm");
+
+    function generateIdeaRow(idea) {
+
+        var tags = idea.tags.split(", ");
+
+        var tagsHTML = "";
+
+        tags.forEach(function (tag) {
+            tagsHTML += '<div class="chip">' + tag + '</div>';
+        });
+
+        return `<div class="row">
+                  <div class="col s12">
+                    <div class="card blue-grey">
+                      <a href="#detailView">
+
+                        <div class="card-content white-text">
+                          <span class="card-title">` + idea.project_name +`</span>
+                          <p>` + idea.project_desc + `</p>
+                        </div>
+
+                        <div class="card-action">
+                            ` + tagsHTML + `
+                              <div class="chip orange white-text pull-right">` + idea.spots + ` Spots Remaining</div>
+                        </div>
+                      </a>
+                    </div>
+                  </div>
+              </div>`;
+    }
 
     $("#addIdea").on('click', function () {
       var formData = ideaForm.serialize();
