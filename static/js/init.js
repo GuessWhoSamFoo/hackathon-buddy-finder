0eb313f3a99fa6719a3c2901dc72e245c9f32649
Bogdan Pshonyak <bpshonyak@live.com>
2016-12-03 16:31:49 -0800

    added get initail posts function

--- static/js/init.js
+++ static/js/init.js
@@ -1,12 +1,31 @@
 (function($){
   $(function(){
 
+    function getInitialPosts() {
+
+      return [
+        {
+          author: "Bogdan P",
+          project_name: "Uber For Fish",
+          project_desc: "Fish need to go places too!",
+          tags: [
+            "PHP",
+            "SQL"
+          ],
+          stops_left: 4
+        }
+      ];
+    }
+
+    var posts = getInitialPosts;
+
     $('.button-collapse').sideNav();
     $("#detailView").modal({
       ready: function(modal, trigger) { // Callback for Modal open. Modal and trigger parameters available.
         console.log(modal, trigger);
       }
     });
+
     $("#createView").modal();
 
   }); // end of document ready
